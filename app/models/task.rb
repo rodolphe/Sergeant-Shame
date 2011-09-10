d5f40a3a61682e9df9d6e40d1f5b9434d2370a3a
Rodolphe Courtier <rodolphe@ifeelgoods.com>
2011-09-10 18:28:45 -0500

    Added place id to task object so that it all works together with the checkin counting method.

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,9 +2,11 @@ class Task < ActiveRecord::Base
   has_many :check_ins
   belongs_to :user
 
-  def count_checkins(set_of_place_ids, time_length)
+  def count_checkins
     access_token = user.fb_access_token
+    set_of_place_ids = Set.new([place_id.to_s])
     times_checkedin = 0
+    time_length = 1.send(period)
     checkins = MiniFB.get(access_token, 'me/checkins')
     checkins['data'].each do |checkin|
       if Time.now - Time.parse(checkin['created_time']) > time_length
@@ -14,6 +16,14 @@ class Task < ActiveRecord::Base
     end
     
     return times_checkedin
+  end
+
+  def we_good
+    if count_checkins < frequency
+      "YOU IDIOT WHAT IS WRONG WITH YOU"
+    else
+      "We good"
+    end
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110910230727) do
+ActiveRecord::Schema.define(:version => 20110910231824) do
 
   create_table "check_ins", :force => true do |t|
     t.integer  "task_id"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20110910230727) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "place_id"
   end
 
   create_table "users", :force => true do |t|
