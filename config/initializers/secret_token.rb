8d0fa077fc9fac874495f5a7941fe617d0637d34
Rodolphe Courtier <rodolphe@ifeelgoods.com>
2011-09-10 16:23:14 -0500

    Initialized repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tc2011::Application.config.secret_token = '4ebc0d85a312abf33d9fdb3db335f39ee7f92e218726798e0102e8586c28e338fec346506d1ec9313501c09d13010cf84ae27c06181e191211d8db1c5ac90a1e'
